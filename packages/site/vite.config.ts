c72498bdc57c682e55b74780055d4659f53d9162
Damien Clarke <dclarke@atlassian.com>
2023-01-27 17:29:55 +1100

    ssr test

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { ssr } from "vite-plugin-ssr/plugin";
+// import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,34 +10,35 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      selfDestroying: true,
-      manifest: {
-        name: "Damien Clarke",
-        short_name: "Damien Clarke",
-        description: "Software Engineer living in Hobart, Tasmania, Australia.",
-        display: "fullscreen",
-        theme_color: "#0e151b",
-        icons: [
-          {
-            src: "android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
+    ssr({ prerender: true }),
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   selfDestroying: true,
+    //   manifest: {
+    //     name: "Damien Clarke",
+    //     short_name: "Damien Clarke",
+    //     description: "Software Engineer living in Hobart, Tasmania, Australia.",
+    //     display: "fullscreen",
+    //     theme_color: "#0e151b",
+    //     icons: [
+    //       {
+    //         src: "android-chrome-192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "android-chrome-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "android-chrome-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any maskable",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
 });
